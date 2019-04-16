49aac61cba47d6f5e227637d2324152b5ed34551
areve <areve@example.com>
2019-04-16 19:44:50 +0100

    only connect once

--- mqtt-publisher.js
+++ mqtt-publisher.js
@@ -1,12 +1,15 @@
 const mqtt = require('mqtt')
 const client = mqtt.connect('wss://areve-beetle.herokuapp.com/mqtt')
+let connected = false
 client.on('connect', () => {
-  setInterval(() => {
-    const message = new Date().toISOString()
-    client.publish('line1', 'hello ' + message)
-    client.publish('line2', 'world ' + message)
-    const trueOrFalse = ~~(Math.random() * 2)
-    client.publish('backlight', trueOrFalse ? 'on': 'off')
-    client.publish('log', 'log from mqtt-publisher ' + message)
-    }, 2000)
+  if (!connected) {
+    setInterval(() => {
+      const message = new Date().toISOString()
+      client.publish('line1', 'hello ' + message)
+      client.publish('line2', 'world ' + message)
+      const trueOrFalse = ~~(Math.random() * 2)
+      client.publish('backlight', trueOrFalse ? 'on': 'off')
+      client.publish('log', 'log from mqtt-publisher ' + message)
+      }, 2000)
+  }
 })
